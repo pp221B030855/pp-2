a5458e8bdbf4e1e44901b77c1c925f5c38d9a6a5
pp2_21B030855 <s_kudaibergenov@kbtu.kz>
2022-04-23 08:55:22 +0600

    racer changed

--- week8-9/week8-9/racer.py
+++ week8-9/week8-9/racer.py
@@ -1,132 +1,176 @@
 import pygame
-pygame.init() #initializes the Pygame
-from pygame.locals import* #import all modules from Pygame
-import random
-import os
-screen = pygame.display.set_mode((798,600))
 
 
-#changing title of the game window
-pygame.display.set_caption('Racing Beast')
+#Imports
+import pygame, sys
+from pygame.locals import *
+import random, time
+ 
+#Initialzing 
+pygame.init()
+ 
+#Setting up FPS 
+FPS = 60
+FramePerSec = pygame.time.Clock()
+ 
+#Creating colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+#Other Variables for use in the program
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+COIN = 0
+ 
+#Setting up Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+ 
+background = pygame.image.load(r'C:\Users\kudai\Desktop\piccc\car game_bg.png')
+background = pygame.transform.scale(background, (400,600))
+
+#Create a white screen 
+DISPLAYSURF = pygame.display.set_mode((400,600))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
 
 
-#defining our gameloop function
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load(r'C:\Users\kudai\Desktop\piccc\car game_car1.png')
+        self.image = pygame.transform.scale(self.image, (150, 50))
+        self.image = pygame.transform.rotate(self.image, 90)
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)  
 
-def gameloop():
-
-    #setting background image
-    bg = pygame.image.load('car game_bg.png')
-    
-    
-    # setting our player
-    maincar = pygame.transform.scale(pygame.image.load(os.path.join('car game_car.png')),(50,150))
-    maincar = pygame.transform.rotate(pygame.transform.scale(maincar, (50,100)),180)
-    maincarX = 350
-    maincarY = 495
-    maincarX_change = 0
-    maincarY_change = 0
-
-    #other cars
-    car1 = pygame.transform.scale(pygame.image.load(os.path.join('car game_car1.png')),(100,50))
-    car1 = pygame.transform.rotate(pygame.transform.scale(car1, (100,50)),90)
-    car1X = random.randint(178,490)
-    car1Y = 100
-
-    car2 = pygame.transform.scale(pygame.image.load(os.path.join('car game_car2.png')),(100,50))
-    car2 = pygame.transform.rotate(pygame.transform.scale(car2, (100,50)),90)
-    car2X = random.randint(178,490)
-    car2Y = 100
-
-    car3 = pygame.transform.scale(pygame.image.load(os.path.join('car game_car3.png')),(100,50))
-    car3 = pygame.transform.rotate(pygame.transform.scale(car3, (100,50)),90)
-    car3X = random.randint(178,490)
-    car3Y = 100
-    
-    
+      def move(self):
+        global SCORE
+        self.rect.move_ip(0,SPEED)
+        if (self.rect.top > 600):
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
 
-   
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
 
-                #checking if any key has been pressed
-            if event.type == pygame.KEYDOWN: 
-                if event.key == pygame.K_RIGHT:
-                    maincarX_change += 2
-            
-                if event.key == pygame.K_LEFT:
-                    maincarX_change -= 2
-                
-                if event.key == pygame.K_UP:
-                    maincarY_change -= 2
-                    
-                if event.key == pygame.K_DOWN:
-                    maincarY_change += 2
 
-                #checking if key has been lifted up
-            if event.type == pygame.KEYUP: 
-                if event.key == pygame.K_RIGHT:
-                    maincarX_change = 0
-            
-                if event.key == pygame.K_LEFT:
-                    maincarX_change = 0
-                
-                if event.key == pygame.K_UP:
-                    maincarY_change = 0
-                    
-                if event.key == pygame.K_DOWN:
-                    maincarY_change = 0            
 
-        #setting boundary for our main car
-        if maincarX < 178:
-            maincarX = 178
-        if maincarX > 490:
-            maincarX = 490
+
+
+class coin(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load(r'C:\Users\kudai\Desktop\piccc\coin.png')
+        self.image = pygame.transform.scale(self.image, (20,20))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)  
+      def move(self):
+        self.rect.move_ip(0,SPEED)
+        if (self.rect.top > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
         
-        if maincarY < 0:
-            maincarY = 0
-        if maincarY > 495:
-            maincarY = 495
-
-
-        #CHANGING COLOR WITH RGB VALUE, RGB = RED, GREEN, BLUE 
-        screen.fill((0,0,0))
-
-        #displaying the background image
-        screen.blit(bg,(0,0))
-
-        #displaying our main car
-        screen.blit(maincar,(maincarX,maincarY))
-
-        #displaing other cars
-        screen.blit(car1,(car1X,car1Y))
-        screen.blit(car2,(car2X,car2Y))
-        screen.blit(car3,(car3X,car3Y))
         
-        #updating the values
-        maincarX += maincarX_change
-        maincarY +=maincarY_change
-
-        #movement of the enemies
-        car1Y += 1
-        car2Y += 1
-        car3Y += 1
 
 
-        #moving enemies infinitely
-        if car1Y > 670:
-            car1Y = -10
-        if car2Y > 670:
-            car2Y = -10
-        if car3Y > 670:
-            car3Y = -10
+    
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load(r"C:\Users\kudai\Desktop\piccc\car game_car.png")
+        self.image = pygame.transform.scale(self.image, (50, 150))
+        self.image = pygame.transform.rotate(self.image, 180)
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+        
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        #if pressed_keys[K_UP]:
+            #self.rect.move_ip(0, -5)
+        #if pressed_keys[K_DOWN]:
+            #self.rect.move_ip(0,5)
+         
+        if self.rect.left > 0:
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(5, 0)
+
+#Setting up Sprites        
+P1 = Player()
+E1 = Enemy()
+C1 = coin()
 
 
+#Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+coins = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(C1)
+all_sprites.add(E1)
 
+    
+
+#Adding a new User event 
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+ 
+#Game Loop
+while True:
+       
+    #Cycles through all events occurring  
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+              SPEED += 0.5     
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    DISPLAYSURF.blit(background, (0,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    Coins = font_small.render(str(COIN), True, RED)
+    DISPLAYSURF.blit(scores, (10,10))
+    DISPLAYSURF.blit(Coins, (30,30))
+ 
+    #Moves and Re-draws all Sprites
+    
+
+    if pygame.sprite.spritecollide(P1, coins, True):
+        COIN += 1
+        C1 = coin()
+        coins.add(C1)
+        all_sprites.add(C1)
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    if pygame.sprite.spritecollideany(P1,[C1]):
+        COIN+=1
+        C1.rect.top = 0
+        C1.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)        
+    #To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+        time.sleep(0.5)
+                    
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30,250))
+           
         pygame.display.update()
-
-
-gameloop()
+        for entity in all_sprites:
+            entity.kill() 
+            time.sleep(2)
+            pygame.quit()
+            sys.exit()        
+         
+    pygame.display.update()
+    FramePerSec.tick(FPS)
